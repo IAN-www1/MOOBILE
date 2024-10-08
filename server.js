69ba934c59d3fcf02cc949e54e9adcb0fd2e36ff
Seeker132002 <seeker130213@gmail.com>
2024-10-08 13:21:39 +0800

    Updated to use dynamic port from Railway

--- server.js
+++ server.js
@@ -70,5 +70,5 @@ app.use('/api/orders/mobile', orderMobileRoutes);
 
 // Start server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
