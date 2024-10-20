901133aaa0a56e850264233fc92cfef183058b21
IAN-www1 <ioppoi5341@gmail.com>
2024-10-21 03:46:54 +0800

    Update Item.js

--- models/Item.js
+++ models/Item.js
@@ -1,5 +1,19 @@
 const mongoose = require('mongoose');
 
+const reviewSchema = new mongoose.Schema({
+  customerName: String,
+  rating: { 
+    type: Number, 
+    min: 1, 
+    max: 5 
+  }, // Rating out of 5
+  comment: String,
+  date: { 
+    type: Date, 
+    default: Date.now 
+  }
+});
+
 const itemSchema = new mongoose.Schema({
   name: String,
   category: String,
@@ -11,6 +25,7 @@ const itemSchema = new mongoose.Schema({
     price: Number,
     _id: mongoose.Schema.Types.ObjectId
   }],
+  reviews: [reviewSchema] // Array of reviews
 });
 
 const Item = mongoose.model('Item', itemSchema);
