4c20ff04b8030d9403e9f04af8c335e1aa9de9bb
IAN-www1 <ioppoi5341@gmail.com>
2024-11-10 22:05:57 +0800

    Update ticket.js

--- models/ticket.js
+++ models/ticket.js
@@ -22,6 +22,15 @@ const ticketSchema = new mongoose.Schema({
     type: String,  // Storing the image as a base64-encoded string
     default: null, // Optional field (can be null if no image is uploaded)
   },
+  userId: { // Make sure userId is here and linked to the Customer
+    type: mongoose.Schema.Types.ObjectId, // Reference to the Customer model
+    ref: 'Customer',
+    required: true,
+  },
+  username: {  // Add username field
+    type: String,
+    required: true,
+  },
   createdAt: {
     type: Date,
     default: Date.now,
@@ -30,8 +39,23 @@ const ticketSchema = new mongoose.Schema({
     type: String,
     default: 'Open', // Ticket is open by default
   },
+  messages: [
+    {
+      message: {
+        type: String,
+        required: true,  // Message content is required
+      },
+      sender: {
+        type: String,
+        default: 'You', // Default sender is 'User'
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      }
+    }
+  ],
 });
-
 // Create and export the Ticket model
 const Ticket = mongoose.model('Ticket', ticketSchema);
 
