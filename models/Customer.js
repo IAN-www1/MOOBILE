3d2c107905298f63349632991ad7b5ca84b4150e
IAN-www1 <ioppoi5341@gmail.com>
2024-11-16 16:23:17 +0800

    Update Customer.js

--- models/Customer.js
+++ models/Customer.js
@@ -8,7 +8,11 @@ const customerSchema = new mongoose.Schema({
   contact: { type: String }, // New field for the customer's contact number
   resetPasswordToken: { type: String }, // Field to store the reset token
   resetPasswordExpires: { type: Date },   // Field to store the expiration date of the reset token
-  playerId: { type: String } // Field for Player ID
+  playerId: { type: String }, // Field for Player ID
+  
+  // New fields for RFID
+  rfidTagId: { type: String, required: false, unique: true }, // RFID tag ID (unique for each customer)
+  balance: { type: Number, default: 0.0 }, // The balance associated with the RFID tag
 });
 
 module.exports = mongoose.model('Customer', customerSchema);
